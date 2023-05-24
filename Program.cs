45f68b76281fb7e7d2dc5ec1498bebcfb9c72585
Anthony Culqui <Anthony Culqui@DESKTOP-L36AQUL>
2023-05-23 22:16:05 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+
+namespace MyFirstSolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+        }
+    }
+}
